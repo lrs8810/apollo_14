d43d6586c3d35ef090ec630474db0fa100a5cec7
Laura Schulz <lrs8810@gmail.com>
2019-10-11 10:33:00 -0600

    Update average_age method spec

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -13,7 +13,7 @@ describe Astronaut, type: :model do
   end
 
   describe 'class methods' do
-    describe '.average_age' do
+    describe '#average_age' do
       @astronaut_1 = Astronaut.create(name: 'Sally Ride', age: 45, job: 'Commander')
       @astronaut_2 = Astronaut.create(name: 'Ellen Ochoa', age: 42, job: 'Physicist')
       @all = Astronaut.all
